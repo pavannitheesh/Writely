4636a749b2dc6c82ca9d85ebd9b5e3b9b174a3bc
pavannitheesh <gpavannitheesh1@gmail.com>
2025-04-28 01:37:11 +0530

    updated hero section

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,8 +1,11 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 const Heading = () => {
+    const { isAuthenticated,isLoading } = useConvexAuth();
     return ( 
         <div className="max-w-3xl space-y-4">
         <h1 className="text-3xl sm:text-5xl md:text-6xl
@@ -15,6 +18,18 @@ const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
         </h3>
+        {isLoading && <div className="flex justify-center w-full items-center">
+        <Spinner size="lg"/>
+        </div>}
+        {
+            !isAuthenticated && !isLoading && (
+                <Button>
+                    <Link href="/documents">
+                Get Started<ArrowRight/>
+                </Link>
+            </Button>
+            )
+        }
         <Button>
             Get Started<ArrowRight/>
         </Button>
