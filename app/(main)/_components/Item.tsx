94ac9820d345d850f43f3f97defd6c7ee075e9de
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-05 23:54:58 +0530

    feat: add archive functionality and dropdown menu to Item component

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import { DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuLabel,
+ } from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -23,7 +31,9 @@ interface ItemProps {
 }
 const Item = ({id,documentIcon,label,icon:Icon,onClick,expanded,onExpand,active,isSearch,level=0}:ItemProps) => {
     const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+    const user=useUser();
     const create=useMutation(api.documents.create);
+    const archive=useMutation(api.documents.archive);
     const router=useRouter();
 
     const handleExpand = (e: React.MouseEvent) => {
@@ -47,7 +57,16 @@ const Item = ({id,documentIcon,label,icon:Icon,onClick,expanded,onExpand,active,
             error:"Error Creating Document"
         });
     }
-
+    const onArchive = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        if(!id) return ;
+        const promise = archive({ id });
+        toast.promise(promise,{
+            loading:"Moving to Trash..",
+            success:"Note moved to trash",
+            error:"Failed to archive note"
+        });
+    }
     return ( 
         <div
         onClick={onClick}
@@ -86,6 +105,21 @@ const Item = ({id,documentIcon,label,icon:Icon,onClick,expanded,onExpand,active,
             {
                 !!id && (
                     <div className="ml-auto flex items-center gap-x-2">
+                       <DropdownMenu>
+                        <DropdownMenuTrigger asChild className="opacity-0 group-hover:opacity-100  h-full rounded-sm bg-neutral-300 dark:bg-neutral-600 mr-2" >
+                            <MoreHorizontal className="h-4 w-4 shrink-0 text-muted-foreground/50"/>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="start" side="right" className="w-60" forceMount>
+                            <DropdownMenuItem onClick={onArchive}>
+                                <Trash className="mr-2 h-4 w-4"/>Delete
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator/>
+                            <div className="px-2 py-1 text-xs text-muted-foreground font-semibold">
+                                last edited by <span className="font-semibold">{user.user?.username}</span>
+                            </div>
+
+                        </DropdownMenuContent>
+                       </DropdownMenu>
                        <div className="opacity-0 group-hover:opacity-100  h-full ml-auto rounded-sm bg-neutral-300 dark:bg-neutral-600 mr-2" onClick={onCreate}>
                         <Plus className="h-4 w-4 shrink-0 text-muted-foreground/50"/>
                        </div>
@@ -102,8 +136,8 @@ Item.Skeleton=function ItemSkeleton({level=0}:{level?:number}) {
     return (
        <div style={{
         paddingLeft: `${(level * 12)+25}px`,}} className="flex gap-x-2 py-[3px]">
-            <Skeleton className="h-4 w-4 " />
-            <Skeleton className="h-4 w-[30%] "/>
+            <Skeleton className="h-8 w-8 " />
+            <Skeleton className="h-8 w-[30%] "/>
        </div>
     )
 }
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,14 +1,39 @@
 import {v} from "convex/values";
 import {mutation,query} from "./_generated/server";
 import {Doc,Id} from "./_generated/dataModel";
-// export const get=query({
-//     handler:async (ctx)=>{
-//         const identity=await ctx.auth.getUserIdentity();
-//         if(!identity) throw new Error("User not authenticated");
-//         const documents=await ctx.db.query("documents").collect();
-//         return documents;
-//     }
-// })
+
+export const archive=mutation({
+    args:{
+        id:v.id("documents"),
+    },
+    handler:async (ctx,args)=>{
+        const {id}=args;
+        const identity=await ctx.auth.getUserIdentity();
+        if(!identity) throw new Error("User not authenticated");
+        const userId=identity.subject;
+        const document=await ctx.db.get(id);
+        if(!document) throw new Error("Document not found");
+        if(document.userId!==userId) throw new Error("You are not authorized to archive this document");
+        const recursiveArchive=async (docId:Id<"documents">)=>{
+            const childDocuments=await ctx.db.query("documents").withIndex("by_parent",(q)=>q
+            .eq("userId",userId)
+            .eq("parentDocument",docId)
+            ).filter((q)=>q.eq(q.field("isArchive"),false)).collect();
+            for(const childDocument of childDocuments){
+                await ctx.db.patch(childDocument._id,{
+                    isArchive:true
+                });
+                await recursiveArchive(childDocument._id);
+            }
+        }
+        recursiveArchive(id);
+
+        const doc=await ctx.db.patch(id,{
+            isArchive:true
+        });
+        return doc;
+    }
+})
 export const getSidebar=query({
     args:{
         parentDocument:v.optional(v.id("documents")),
