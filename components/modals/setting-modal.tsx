703d758d156dbdfdcdd24ecd473fffe94afd4628
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-08 22:52:42 +0530

    renamed setting-modal.tsx

---
+++ components/modals/setting-modal.tsx
@@ -0,0 +1,43 @@
+import { useSetting } from "@/hooks/use-setting";
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { ModeToggle } from "../mode-toggle";
+
+export const SettingsModal=()=>{
+    const settings =useSetting();
+
+    return (
+        <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+            <DialogContent>
+                <DialogHeader className="border-b pb-3">
+                    <DialogTitle>Settings</DialogTitle>
+                    <DialogDescription>
+                        Change your settings here. You can always change them back.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col gap-y-1">
+                        <Label>Appearnace</Label>
+                      <span className="text-[0.8rem] text-muted-foreground">Customize how Jotion looks on your device</span>
+                    </div>
+                    <ModeToggle/>
+                </div>
+                <DialogFooter className="justify-between">
+                    <Button variant="outline" onClick={settings.onClose}>
+                        Cancel
+                    </Button>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+
+}
