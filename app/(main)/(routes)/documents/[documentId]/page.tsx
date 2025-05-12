7569fd58eb5215545f06b96136009e313bbac664
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-13 00:14:33 +0530

    added error page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Cover } from "@/components/cover";
-import Editor from "@/components/editor";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 import Toolbar from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
@@ -26,6 +27,8 @@ const DocumentIdPage = () => {
         })
         
     }
+
+    const Editor =useMemo(()=>dynamic(()=>import("@/components/editor"),{ssr:false}),[]);
     if(document===undefined) {
         return (
             <div>
